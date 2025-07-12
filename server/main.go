8d0e5b401b4fb48755fc5400660a689b3f4e92cd
Duane Evans <duanesevans@gmail.com>
2025-07-12 09:58:37 -0700

    update to test ci/cd

--- server/main.go
+++ server/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "The AI Vibe DJ is listening...")
+		fmt.Fprintf(w, "<span style='color:red;'>The AI Vibe DJ is listening...</span>")
 	})
 
 	log.Println("Starting server on :8080")
